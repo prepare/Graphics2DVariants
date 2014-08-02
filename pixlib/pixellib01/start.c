1f5c4a0919d74a3a8e3d6c9ffd4f2cb9f6c00bbb
win <wintercoredev@gmail.com>
2014-08-02 19:44:26 +0700

    06:

--- pixlib/pixellib01/start.c
+++ pixlib/pixellib01/start.c
@@ -1,9 +1,9 @@
 
 int main(void)
 {
-	//main1();//test_image_transform
-	//main2();//test_image_drawing
-	//main3();//test_drawing
-	main4();//test_anti_alias
+	//main1() 
+	//main2(); 
+	//main3(); 
+	//main4(); 
 	return 0;
 }
